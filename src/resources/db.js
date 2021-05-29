3190c6be34e39d64a62b1935ed83ad61f3059347
schuklin_pavel <ininin@vivaldi.net>
2021-05-30 01:19:00 +0400

    jsdoc db.js

--- src/resources/db.js
+++ src/resources/db.js
@@ -5,12 +5,34 @@ const TABLES = {
 }
 
 const db = {
+  /**
+   * Return table
+   * @param {string} table name
+   * @returns {User[]|Task[]|Board[]} table
+   */
   getAll: (table) => TABLES[table],
 
+  /**
+   * Return table element by id
+   * @param {string} table name
+   * @param {string} id element
+   * @returns {User|Task|Board} entity
+   */
   getById: (table, id) => TABLES[table].find(i => i.id === id),
 
+  /**
+   * Add entity to table
+   * @param {string} table name
+   * @param {User|Task|Board} item entity
+   */
   post: (table, item) => TABLES[table].push(item),
 
+  /**
+   * Update entity in table
+   * @param {string} table name
+   * @param {User|Task|Board} item entity
+   * @returns {User|Task|Board} entity
+   */
   put: (table, item) => {
     const index = TABLES[table].findIndex(i => i.id === item.id);
     // eslint-disable-next-line
@@ -22,6 +44,11 @@ const db = {
     return TABLES[table][index];
   },
 
+  /**
+   * Remove entity by id
+   * @param {string} table name
+   * @param {string} id entity
+   */
   delete: (table, id) => {
     if (table === 'BOARDS') {
       TABLES.TASKS = TABLES.TASKS.filter(task => task.boardId !== id);
